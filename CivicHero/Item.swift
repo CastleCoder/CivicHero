1cf8f98495a949b6eea97c213bb8cf43c5b98075
CastleCoder <cyrillechateau@icloud.com>
2024-03-16 11:49:38 +0100

    Initial Commit

---
+++ CivicHero/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CivicHero
+//
+//  Created by Cyrille Chateau on 16/03/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
